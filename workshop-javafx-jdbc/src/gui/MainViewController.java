c96d4ce619a07b4d796ec24eb86936688b0461f8
jonathanrichardss <jonathanrichard45@hotmail.com>
2023-05-11 00:39:09 -0300

    Adding database access.

--- workshop-javafx-jdbc/src/gui/MainViewController.java
+++ workshop-javafx-jdbc/src/gui/MainViewController.java
@@ -3,6 +3,7 @@ package gui;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.function.Consumer;
 
 import application.Main;
 import gui.util.Alerts;
@@ -15,6 +16,7 @@ import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.VBox;
+import model.services.DepartmentService;
 
 public class MainViewController implements Initializable {
 
@@ -35,12 +37,15 @@ public class MainViewController implements Initializable {
 	
 	@FXML
 	private void onMenuItemDepartmentAction() {
-		loadView("/gui/DepartmentList.fxml");
+		loadView("/gui/DepartmentList.fxml", (DepartmentListController controller) -> {
+			controller.setDepartmentService(new DepartmentService());
+			controller.updateTableView();
+		});
 	}
 	
 	@FXML
 	private void onMenuItemAboutAction() {
-		loadView("/gui/AboutView.fxml");
+		loadView("/gui/AboutView.fxml", x -> {});
 	}
 	
 	
@@ -50,7 +55,7 @@ public class MainViewController implements Initializable {
 		
 	}
 	
-	public synchronized void loadView(String absoluteName) {
+	public synchronized <T> void loadView(String absoluteName, Consumer<T> initializingAction) {
 		FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(absoluteName));
 		try {
 			VBox newVBox = fxmlLoader.load();
@@ -63,6 +68,9 @@ public class MainViewController implements Initializable {
 			mainVbox.getChildren().clear();
 			mainVbox.getChildren().add(mainMenu);
 			mainVbox.getChildren().addAll(newVBox.getChildren());
+			
+			T controller = fxmlLoader.getController();
+			initializingAction.accept(controller);
 			 
 			
 		} catch (IOException e) {
--- workshop-javafx-jdbc/src/model/services/DepartmentService.java
+++ workshop-javafx-jdbc/src/model/services/DepartmentService.java
@@ -3,16 +3,22 @@ package model.services;
 import java.util.ArrayList;
 import java.util.List;
 
+import model.dao.DaoFactory;
+import model.dao.DepartmentDao;
 import model.entities.Department;
 
 public class DepartmentService {
+	
+	DepartmentDao dao = DaoFactory.createDepartmentDao(); 
 
 	public List<Department> findAll() {
-		List<Department> list = new ArrayList<>();
-		
-		list.add(new Department(1, "Tech"));
-		list.add(new Department(2, "Fruits"));
-		list.add(new Department(3, "Home Sweet Home"));
-		return list;
+//		List<Department> list = new ArrayList<>();
+//		
+//		list.add(new Department(1, "Tech"));
+//		list.add(new Department(2, "Fruits"));
+//		list.add(new Department(3, "Home Sweet Home"));
+//		System.out.println("Hey!!!");
+//		dao.findAll().forEach(e -> System.out.println(e.getName()));
+		return dao.findAll();
 	}
 }
